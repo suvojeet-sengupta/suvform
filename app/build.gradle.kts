5d8281d97b0f8909f79cfa6a95afde63dff2be50
suvojitsengupta21@gmail.com <suvojitsengupta21@gmail.com>
2026-05-20 12:44:37 +0530

    bomp compile sdk to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.suvojeetsengupta.suvform"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.suvojeetsengupta.suvform"
